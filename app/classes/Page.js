72f8d934a7e66db1e8d937da34c01eab9a54cbb9
goodness <goodness@revivar.app>
2023-01-28 11:45:35 +0100

    add a check for Canvas

--- app/classes/Page.js
+++ app/classes/Page.js
@@ -25,7 +25,7 @@ export default class Page {
     this.createMesh?.call(this);
   }
   predestroy() {
-    Canvas.navigate = () => {
+    Canvas?.navigate = () => {
       this.removeEventListeners && this.removeEventListeners();
       gsap.to(this.element, { autoAlpha: 0 });
     };
