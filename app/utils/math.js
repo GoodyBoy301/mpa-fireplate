82bc79e6d73d6f28695d52be363dc8e9cc19036f
goodness <goodness@revivar.app>
2023-01-28 09:57:31 +0100

    update: add more functions

--- app/utils/math.js
+++ app/utils/math.js
@@ -12,6 +12,46 @@ export function random(min, max) {
   return gsap.utils.random(min, max);
 }
 
+export function asyncRandom(limit, timeout) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Math.random() * limit);
+    }, timeout);
+  });
+}
+
 export function map(valueToMap, inMin, inMax, outMin, outMax) {
   return gsap.utils.mapRange(inMin, inMax, outMin, outMax, valueToMap);
+}
+
+export function pixelToRem(pixels, innerWidth = window.innerWidth) {
+  return (innerWidth / 1512) * 10 * pixels;
+}
+
+export function snap(
+  value,
+  lowerLimit,
+  upperLimit,
+  fractStart = 0,
+  fractEnd = 6
+) {
+  let normal = map(value, lowerLimit, upperLimit, fractStart, fractEnd);
+  normal = Math.round(normal);
+  const snapTo = (normal / fractEnd) * upperLimit;
+  return snapTo;
+}
+
+export function snapTo(
+  normal,
+  upperLimit,
+  lowerLimit,
+  fractStart = 0,
+  fractEnd = 6
+) {
+  if (!lowerLimit) {
+    return (normal / fractEnd) * upperLimit;
+  } else {
+    const snapTo = (normal / fractEnd) * upperLimit;
+    return map(snapTo, fractStart, upperLimit, lowerLimit, upperLimit);
+  }
 }
