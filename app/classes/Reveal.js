8a47f01aedca9016265aef868887ffca914d9c19
Goodness Urama <87700025+GoodyBoy301@users.noreply.github.com>
2023-08-26 17:46:31 +0100

    Update Reveal.js

--- app/classes/Reveal.js
+++ app/classes/Reveal.js
@@ -1,60 +1,44 @@
-import Component from "classes/Component";
-import gsap from "gsap";
+import Component from "classes/Component"
 
 export default class Reveal extends Component {
   constructor(params) {
-    super(params);
-    this.threshold = params.threshold || 0.75;
-    this.unobserve = params.unobserve ?? true;
-    this.createObserver();
+    super(params)
+    this.threshold = params.threshold || 0.15
+    this.unobserve = params.unobserve ?? true
+    this.params = params
+    this.createObserver()
   }
 
   createObserver() {
-    const options = { threshold: this.threshold };
+    const options = { threshold: this.threshold }
 
     this.observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          try {
-            this[entry.target.getAttribute("data-reveal")](entry.target)
-          } catch {
-            console.log(entry.target)
-          }
-          if (this.unobserve) this.observer.unobserve(entry.target);
-          this.observer.unobserve(entry.target);
+          const el = entry.target
+          this.dispatchEvent({
+            type: "in",
+            index: entry.target.getAttribute("data-slide"),
+            target: el,
+          })
+          if (!this.params.keep) this.observer.unobserve(entry.target)
         } else {
-          this[entry.getAttribute("data-reveal") + "Reverse"]?.call(
-            this,
-            entry.target
-          );
+          const el = entry.target
+          this.dispatchEvent({
+            type: "out",
+            index: entry.target.getAttribute("data-slide"),
+            target: el,
+          })
         }
-      });
-    }, options);
+      })
+    }, options)
 
     Object.entries(this.elements).forEach(([key, value]) => {
       if (value.forEach) {
         value.forEach((element) => {
-          this.observer.observe(element);
-        });
-      } else this.observer.observe(value);
-    });
-  }
-
-  curtain(element) {
-    gsap.fromTo(
-      element,
-      {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-        autoAlpha: 1,
-        scale: 1.05,
-      },
-      {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-        autoAlpha: 1,
-        scale: 1,
-        delay: element.getAttribute("data-delay"),
-        duration: 0.5,
-      }
-    );
+          this.observer.observe(element)
+        })
+      } else this.observer.observe(value)
+    })
   }
 }
